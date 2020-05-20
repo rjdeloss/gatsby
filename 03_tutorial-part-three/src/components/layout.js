bf691590cdb00f4641aa536f049af52aab270130
Droool <rjdeloss@gmail.com>
2020-05-20 11:23:48 -0400

    modify layout for title

--- 03_tutorial-part-three/src/components/layout.js
+++ 03_tutorial-part-three/src/components/layout.js
@@ -2,7 +2,8 @@ import React from "react"
 
 export default function Layout({ children }) {
   return (
-    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+      <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+          <h3>MySweetSite</h3>
       {children}
     </div>
   )
